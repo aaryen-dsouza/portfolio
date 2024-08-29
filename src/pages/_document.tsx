4f630e20304103caa5490d393179ae5890b5c4f9
Aaryen D'souza <dc.aaryen@gmail.com>
2024-08-29 19:02:36 -0400

    Google Fonts imports changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+          href="https://fonts.googleapis.com/css2?family=Rubik+Iso&family=Source+Code+Pro:wght@400;500;700&family=VT323&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kumar+One+Outline&display=swap"
+          rel="stylesheet"
+        />
       <body>
         <Main />
         <NextScript />
